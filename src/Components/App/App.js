ec374e9c070058dd9102a428a361db4d610c6504
nevelson <nevelson92@gmail.com>
2020-06-30 12:05:00 -0400

    Adding routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import ChallengeRoute from "./../../Routes/ChallengeRoute/ChallengeRoute";
 import "./App.css";
 
 class App extends Component {
-  state = { hasError: false };
-
   render() {
     return (
       <div className="App">
         <header>{/* ToDo */}</header>
         <main>
           <p>test</p>
-          {/* <Switch></Switch> */}
+          <Switch>
+            <Route exact path={"/challenge"} component={ChallengeRoute} />
+          </Switch>
         </main>
       </div>
     );
