383f893f5863a5273eecdc67054e9c83c28475cd
Ren F <renfinn89@gmail.com>
2020-06-30 13:05:58 -0700

    refactored Registration render

--- src/Components/Registration Form/RegistrationForm.js
+++ src/Components/Registration Form/RegistrationForm.js
@@ -22,52 +22,67 @@ class RegistrationForm extends Component {
     e.preventDefault();
     console.log('I am working');
   };
+  renderUsername = () => {
+    return (
+      <div className="reg-account">
+        {this.state.error !== '' && (
+          <h3 className="error">{this.state.error}</h3>
+        )}
+        <Label htmlFor="reg-input username">
+          {' '}
+          <Required />
+          Create Username:
+        </Label>
+        <Input className="reg-input username" required />
+        <Label htmlFor="reg-input email">
+          <Required />
+          Enter Email:
+        </Label>
+        <Input className="reg-input email" required />
+      </div>
+    );
+  };
+  renderPassword = () => {
+    return (
+      <div className="reg-account">
+        {this.state.error !== '' && (
+          <h3 className="error">{this.state.error}</h3>
+        )}
+        <Label htmlFor="reg-input password">
+          <Required />
+          Password:
+        </Label>
+        <Input className="reg-input password" required />
+        <Label htmlFor="reg-input repassword">
+          <Required />
+          Re-enter Password:
+        </Label>
+        <Input className="reg-input repassword" required />
+      </div>
+    );
+  };
+  renderAvatar = () => {
+    return (
+      <div className="reg-account">
+        <Label>Choose Avatar:</Label>
+        <select>
+          <option>Red Mage</option>
+          <option>Blue Mage</option>
+        </select>
+      </div>
+    );
+  };
 
   render() {
     return (
       <div className="reg-container">
         <form className="reg-form" onSubmit={(e) => this.handleSubmit(e)}>
           {this.state.step === 0 ? (
-            <div className="reg-account">
-              {this.state.error !== '' && (
-                <h3 className="error">{this.state.error}</h3>
-              )}
-              <Label htmlFor="reg-input username">
-                {' '}
-                <Required />
-                Create Username:
-              </Label>
-              <Input className="reg-input username" required />
-              <Label htmlFor="reg-input email">
-                <Required />
-                Enter Email:
-              </Label>
-              <Input className="reg-input email" required />
-            </div>
-          ) : this.state.step === 2 ? (
-            <div className="reg-account">
-              <Label>Choose Avatar:</Label>
-              <select>
-                <option>Red Mage</option>
-                <option>Blue Mage</option>
-              </select>
-            </div>
+            <div>{this.renderUsername()}</div>
+          ) : this.state.step === 1 ? (
+            <div>{this.renderPassword()}</div>
           ) : (
-            <div className="reg-account">
-              {this.state.error !== '' && (
-                <h3 className="error">{this.state.error}</h3>
-              )}
-              <Label htmlFor="reg-input password">
-                <Required />
-                Password:
-              </Label>
-              <Input className="reg-input password" required />
-              <Label htmlFor="reg-input repassword">
-                <Required />
-                Re-enter Password:
-              </Label>
-              <Input className="reg-input repassword" required />
-            </div>
+            <div>{this.renderAvatar()}</div>
           )}
           {this.state.step === 2 ? (
             <Button className="reg-btn" type="submit">
