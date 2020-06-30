6a3305a7dce87be8590f745d4ab102a5110697cf
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-06-30 13:30:43 -0700

    created dashboard with analytics tab

---
+++ src/Components/Start/Start.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Start.Module.css'
+export default class Start extends React.Component {
+
+    render() {
+        return (
+            <div className = 'start-menu'>
+                <div> Welcome Click start to begin playing</div>
+                <button> Start </button> 
+            </div>
+
+        )
+    }
+}
