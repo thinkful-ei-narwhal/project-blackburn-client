d875cd9b23af7819af0cc263c616fdf4445b0bff
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-07-01 09:47:57 -0700

    forgot to render landing page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,7 +6,7 @@ import RegistrationRoute from '../../Routes/RegistrationRoute/RegistrationRoute'
 import LoginRoute from '../../Routes/LoginRoute/LoginRoute';
 import SettingsRoute from '../../Routes/SettingsRoute/SettingsRoute';
 import Dashboard from '../Dashboard/Dashboard'
-import LandingPage from '../LandingPage/LandingPage';
+import LandingPageRoute from '../../Routes/LandingPage/LandingPageRoute';
 
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
         <header>{/* ToDo */}</header>
         <main>
           <Switch>
-            <Route exact path={'/'} component={LandingPage} />
+            <Route exact path={'/'} component={LandingPageRoute} />
             <Route exact path={'/challenge'} component={ChallengeRoute} />
             <Route exact path={'/registration'} component={RegistrationRoute} />
             <Route exact path={'/login'} component={LoginRoute} />
---
+++ src/Routes/LandingPage/LandingPageRoute.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from '../../Components/LandingPage/LandingPage'
+
+export default class LandingPageRoute extends React.Component {
+
+
+    render() {
+        return (
+            <LandingPage />
+        )
+    }
+}
