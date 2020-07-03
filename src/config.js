e1d3854079b1a12453c0f6689b7d19c4b351a2dd
nevelson <nevelson92@gmail.com>
2020-07-03 13:03:28 -0400

    Remove alt option

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT:
-    process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000/api",
+    process.env.REACT_APP_API_ENDPOINT,
   API_KEY: process.env.REACT_APP_API_KEY,
 };
