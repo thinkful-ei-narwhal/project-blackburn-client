5ba102cdd9af7278bcd6fdf77b962f674021724d
Ren F <renfinn89@gmail.com>
2020-07-08 10:39:51 -0700

    bkg music working

--- src/Components/Analytics/LineChart.js
+++ src/Components/Analytics/LineChart.js
@@ -5,52 +5,6 @@ import './LineChart.css';
 
 export default class LineChart extends React.Component {
   static contextType = BlackBurnContext;
-  scoreLogic = () => {
-    const { myScores } = this.props;
-    const formatData = myScores.map((data) => {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      const newDate = new Date(data.date).toLocaleDateString('en-US', options);
-      return { date: newDate, score: data.score, wpm: data.wpm };
-    });
-    // let filteredScore = formatData.filter((data, i) => {
-    //   let topScore = {};
-    //   let prevDate = 0;
-    //   if (prevDate === 0 && i > 0) {
-    //     prevDate = formatData[i - 1].date;
-    //   }
-    //   if (i - 1 >= 0 || data.date === prevDate) {
-    //     prevDate = formatData[i - 1].date;
-    //     let prevScore = formatData[i - 1].score;
-    //     let currentScore = data.score;
-
-    //     if (prevScore > currentScore) {
-    //       topScore = prevScore;
-    //     }
-    //   }
-    // });
-    for (let i = 0; i <= formatData.length; i++) {
-      let topScore = {};
-      let prevIndex = formatData[i];
-      let filteredScore = [];
-      if (prevIndex === 0 && i > 0) {
-        prevIndex = formatData[i - 1].date;
-      }
-      if (i - 1 >= 0 || formatData[i].date === prevIndex.date) {
-        prevIndex = formatData[i - 1];
-        console.log(prevIndex);
-        let currentScore = formatData[i].score;
-
-        if (prevIndex.score > currentScore) {
-          topScore = prevIndex;
-          console.log('i trigger', topScore);
-        }
-      }
-      //    else {
-      //     filteredScore.push(topScore);
-      //      console.log(filteredScore);
-      //   }
-    }
-  };
 
   render() {
     const formatData = this.props.myScores.map((data) => {
